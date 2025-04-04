f3175eadf515a1027dcebc733dadc9f974a3fd32
Eldar Radovici <eldar@radovici.com>
2025-04-04 13:06:50 -0400

    Same back to "Metadata".

--- DataModels.EntityFramework/Configurations/Metadata/TargetConfiguration.cs
+++ DataModels.EntityFramework/Configurations/Metadata/TargetConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataModels.Metadata;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DataModels.EntityFramework.Configurations.SecurityMaster
