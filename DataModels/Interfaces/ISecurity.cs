b7dcedc66f8453eb4e435189e2566b7e27c3b811
Eldar Radovici <eldar@radovici.com>
2025-04-04 12:58:42 -0400

    Review ISecurity.

--- DataModels/Interfaces/ISecurity.cs
+++ DataModels/Interfaces/ISecurity.cs
@@ -6,12 +6,13 @@ namespace DataModels.Interfaces
     {
         ISecurity? UnderlyingSecurity { get; }
 
-        SecurityType SecurityType { get; }
+        SecurityType SecurityType { get; } // TODO: should also be an interface
 
         string Symbol { get; }
 
         double Multiplier { get; }
 
+        // NOTE: this was DW's 2019 codebase; commented out for the time-being.
         //SortedDictionary<DateTime, IPrice> Prices { get; }
         //SortedDictionary<DateTime, double> Returns { get; }
         //SortedDictionary<DateTime, IEquitySplit> Splits { get; }
@@ -31,6 +32,6 @@ namespace DataModels.Interfaces
 
         IExchange Exchange { get; }
 
-        ICollection<ISecurityAttribute> SecurityAttributes { get; }
+        IEnumerable<ISecurityAttribute> SecurityAttributes { get; }
     }
 }
