72b4489f67881f1ab9e91da706b4988133cfd53a
Eldar Radovici <eldar@radovici.com>
2025-04-05 12:45:46 -0400

    Reorg'ed security.

--- DataModels/Models/SecurityMaster/Security.cs
+++ DataModels/Models/SecurityMaster/Security.cs
@@ -19,17 +19,13 @@ public partial class Security : ISecurity
 
     public DateTime CreateDateTime { get; private set; }
 
-    public ICollection<SecurityAttribute> SecurityAttributes { get; private set; } = new List<SecurityAttribute>();
-
-    IEnumerable<ISecurityAttribute> ISecurity.SecurityAttributes => SecurityAttributes.Cast<ISecurityAttribute>();
-
     public virtual SecurityType SecurityType { get; private set; } = null!;
 
     public virtual ISecurity? UnderlyingSecurity { get; }
 
-    public string? Display { get; } // TODO: should be name, i.e., nameable (instead of displayable)
+    public ICollection<SecurityAttribute> SecurityAttributes { get; private set; } = new List<SecurityAttribute>();
 
-    public double Multiplier => 1; // NOTE: this changes logically depending on security type (although may hae to be overridden and database driven)
+    IEnumerable<ISecurityAttribute> ISecurity.SecurityAttributes => SecurityAttributes.Cast<ISecurityAttribute>();
 
     public Exchange Exchange { get; private set; } = null!; // maybe better just to expose normal casing and expose UDM mdoels publicly
 
@@ -38,6 +34,10 @@ public partial class Security : ISecurity
     public ICountry Country { get; } = null!;
 
     public ICurrency Currency => Exchange.Country?.Currency ?? DataModels.SecurityMaster.Currency.Empty;
+
+    public string? Display { get; } // TODO: should be name, i.e., nameable (instead of displayable)
+
+    public double Multiplier => 1; // NOTE: this changes logically depending on security type (although may hae to be overridden and database driven)
 
     public string Name => Symbol;
 }
