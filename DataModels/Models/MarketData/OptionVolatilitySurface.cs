e34c52cabbb8b5b796f62cb9162bd4ad29c298e1
Eldar Radovici <eldar@radovici.com>
2025-04-05 12:48:04 -0400

    Updated market data entities.

--- DataModels/Models/MarketData/OptionVolatilitySurface.cs
+++ DataModels/Models/MarketData/OptionVolatilitySurface.cs
@@ -1,23 +1,24 @@
 ﻿namespace DataModels.MarketData;
 
-public partial class OptionVolatilitySurface {
-    public int SecurityId { get; set; }
+public partial class OptionVolatilitySurface
+{
+    public int SecurityId { get; private set; }
 
-    public DateOnly Date { get; set; }
+    public DateOnly Date { get; private set; }
 
-    public double Slope { get; set; }
+    public double Slope { get; private set; }
 
-    public double Derivative { get; set; }
+    public double Derivative { get; private set; }
 
-    public double InfiniteSlope { get; set; }
+    public double InfiniteSlope { get; private set; }
 
-    public double InfiniteDerivative { get; set; }
+    public double InfiniteDerivative { get; private set; }
 
-    public double AtTheMoneyImpliedVolatility1Month { get; set; } // TODO: review this; is this residual from the previous data provider?
+    public double AtTheMoneyImpliedVolatility1Month { get; private set; } // TODO: review this; is this residual from the previous data provider?
 
-    public double AtTheMoneyImpliedVolatility2Month { get; set; }
+    public double AtTheMoneyImpliedVolatility2Month { get; private set; }
 
-    public double AtTheMoneyImpliedVolatility3Month { get; set; }
+    public double AtTheMoneyImpliedVolatility3Month { get; private set; }
 
-    public double AtTheMoneyImpliedVolatility4Month { get; set; }
+    public double AtTheMoneyImpliedVolatility4Month { get; private set; }
 }
--- DataModels/Models/MarketData/Source.cs
+++ DataModels/Models/MarketData/Source.cs
@@ -1,9 +1,10 @@
 ﻿namespace DataModels.MarketData;
 
-public partial class Source {
-    public int Id { get; set; }
+public partial class Source
+{
+    public int Id { get; private set; }
 
-    public string Name { get; set; } = null!;
+    public string Name { get; private set; } = null!;
 
-    public int Priority { get; set; }
+    public int Priority { get; private set; }
 }
