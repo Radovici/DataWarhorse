123e007c117db12a93c7ca47fc8d0018cd1b95ff
Eldar Radovici <eldar@radovici.com>
2025-04-11 12:20:09 -0400

    Use Security namespaces instead of Securities.

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -2,7 +2,7 @@
 using DataModels.EntityFramework.SecurityMaster.Contexts;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Services.Securities;
+using Services.Security;
 
 namespace Tester;
 
