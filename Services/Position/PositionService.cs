089b95b394f0de67f36e5e845b1545eda4b2a8ca
Eldar Radovici <eldar@radovici.com>
2025-04-13 07:41:33 -0400

    Added positionService.

---
+++ Services/Position/PositionService.cs
@@ -0,0 +1,15 @@
+﻿using DataModels.EntityFramework.PositionInformation.Contexts;
+using DataModels.PositionData;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Services.Position
+{
+    public class PositionService(PositionDataContext positionDataContext)
+    {
+        public async Task<IEnumerable<Trade>> GetTradesAsync()
+        {
+            return await positionDataContext.Trades.ToListAsync();
+        }
+    }
+}
